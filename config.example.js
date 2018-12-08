f1d4f9b0a66c2a223d7548db5f7b09d451760633
Rebel <johnhartigan@ya.ru>
2018-12-08 18:51:52 +0200

    Updated package json.

--- config.example.js
+++ config.example.js
@@ -1,10 +1,10 @@
 module.exports = {
-    app: {
-        host: 'localhost',
-        port: 3000
-    },
-    api: {
-        host: 'localhost',
-        port: 8000
-    }
+  app: {
+    host: "localhost",
+    port: 3000
+  },
+  api: {
+    host: "localhost",
+    port: 8000
+  }
 };
