fe9e7c20dd6f50cbf49019d93399849a3aff20a0
Volodymyr Rebel <johnhartigan@ya.ru>
2020-10-21 01:53:18 +0300

    add integration test for custom writable

---
+++ api/helpers/custom-writable.test.ts
@@ -0,0 +1,81 @@
+import { assert } from 'chai';
+import { Readable } from 'stream';
+import { CustomWritable } from './custom-writable';
+
+describe('CustomWritable integration test', () => {
+  before(() => {});
+
+  after(() => {});
+
+  context('when CustomWritable simulated speed is not set', () => {
+    const fileSize = 30 * 1024 * 1024;
+
+    const startTime = new Date();
+    let endTime: Date;
+
+    before(async () => {
+      const readable = Readable.from(Buffer.alloc(fileSize));
+
+      const writable = new CustomWritable({
+        fileSize,
+      });
+
+      readable.pipe(writable);
+
+      await new Promise((resolve, reject) => {
+        writable.on('error', reject);
+
+        writable.on('finish', resolve);
+      });
+
+      endTime = new Date();
+    });
+
+    after(() => {});
+
+    it('should write as expected', () => {
+      const testTime = endTime.valueOf() - startTime.valueOf();
+
+      assert.isBelow(testTime, 1 * 1000);
+    });
+  });
+
+  context('when CustomWritable simulated speed is set to a specific value', function() {
+    const timeout = 10 * 1000;
+
+    this.timeout(timeout);
+
+    const fileSize = 1 * 1024 * 1024;
+
+    const startTime = new Date();
+    let endTime: Date;
+
+    before(async () => {
+      const readable = Readable.from(Buffer.alloc(fileSize));
+
+      const writable = new CustomWritable({
+        fileSize,
+        simulatedSpeed: 128,
+      });
+
+      readable.pipe(writable);
+
+      await new Promise((resolve, reject) => {
+        writable.on('error', reject);
+
+        writable.on('finish', resolve);
+      });
+
+      endTime = new Date();
+    });
+
+    after(() => {});
+
+    it('should write as expected', () => {
+      const testTime = endTime.valueOf() - startTime.valueOf();
+
+      assert.isAbove(testTime, 7 * 1000);
+      assert.isBelow(testTime, 10 * 1000);
+    });
+  });
+});
