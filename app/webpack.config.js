9e2643052622eef0d55be1109fb41652d7dcfbca
Volodymyr Rebel <johnhartigan@ya.ru>
2020-11-12 17:24:47 +0200

    style fix

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 const config = require('./config');
 
 module.exports = {
